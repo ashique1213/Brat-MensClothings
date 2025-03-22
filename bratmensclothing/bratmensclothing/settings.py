67c7fde8d88ea988392c3c3b1d370a81964ef47b
ashique1213 <muhammedashique1213@gmail.com>
2025-03-22 12:10:49 +0530

    For Render

--- bratmensclothing/bratmensclothing/settings.py
+++ bratmensclothing/bratmensclothing/settings.py
@@ -118,13 +118,6 @@ DATABASES = {
 
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
