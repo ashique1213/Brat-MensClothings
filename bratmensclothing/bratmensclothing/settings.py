cfc17fe5234a396029682b0f5d644b8f4bdcc015
ashique1213 <muhammedashique1213@gmail.com>
2025-03-22 12:46:32 +0530

    For Render

--- bratmensclothing/bratmensclothing/settings.py
+++ bratmensclothing/bratmensclothing/settings.py
@@ -105,16 +105,24 @@ WSGI_APPLICATION = 'bratmensclothing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+
+POSTGRES_LOCALLY = False  # Set this to True if running locally
+
+if os.getenv('ENVIRONMENT') == 'production' or POSTGRES_LOCALLY:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
+        }
+    }
 
 
 
